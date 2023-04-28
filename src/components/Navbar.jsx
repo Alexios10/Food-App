c1ff8e87c444c8bfb418413ad6409d03ace5d5b9
Alexios10 <sattar_saud@hotmail.com>
2023-04-28 18:02:33 +0200

    changed the cart bg and color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = () => {
       </div>
 
       {/* Cart button */}
-      <button className="bg-black text-white hidden md:flex items-center py-2 rounded-full">
+      <button className="bg-cyan-800 text-white hidden md:flex items-center py-2 rounded-full">
         <BsFillCartFill size={20} className="mr-2" /> Cart
       </button>
 
